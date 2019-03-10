76756c642b1c9ea3444049026bf3c0c0fd280b7a
FalconXI <dutoanle1611@gmail.com>
2019-03-10 08:07:54 +0700

    commit init

---
+++ PasswordManager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * <pre>
+ * <p>[Summary]</p>
+ * Password Manager Class
+ * </pre>
+ *
+ * @author ToanLD3
+ */
+
+namespace Manager;
+
+
+class PasswordManager
+{
+
+}
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+        }
+
+        li a {
+            display: block;
+            color: #000;
+            padding: 8px 16px;
+            text-decoration: none;
+        }
+
+        /* Change the link color on hover */
+        li a:hover {
+            background-color: #555;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Password Manager</h2>
+
+<ul>
+    <li><a href="#">New User</a></li>
+    <li><a href="#">Validate Password</a></li>
+    <li><a href="#">Login</a></li>
+    <li><a href="#">Change Password</a></li>
+</ul>
+
+</body>
+</html>
