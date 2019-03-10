ed6491e8c8b0a2b3ed4b26f65cc4d5b7ecc34dcc
FalconXI <dutoanle1611@gmail.com>
2019-03-10 11:14:29 +0700

    commit init

---
+++ MainProcess.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: COMPUTER
+ * Date: 10-Mar-19
+ * Time: 11:13
+ */
--- PasswordManager.php
+++ PasswordManager.php
@@ -5,7 +5,7 @@
  * Password Manager Class
  * </pre>
  *
- * @author ToanLD3
+ * @author DucToanLe
  */
 
 namespace Manager;
@@ -13,5 +13,97 @@ namespace Manager;
 
 class PasswordManager
 {
+    public $username;
+    public $password;
 
+    /**
+     * <pre>
+     * <p>[Summary]</p>
+     * Encrypt data
+     * </pre>
+     * @param string $password password
+     * @return string $passwordEncrypt
+     */
+    protected function encrypt($password)
+    {
+        $passwordEncrypt = md5($password);
+
+        return $passwordEncrypt;
+    }
+
+    /**
+     * <pre>
+     * <p>[Summary]</p>
+     * Verify Password
+     * </pre>
+     * @param string $password password
+     * @return boolean true: if password correct
+     *                         false: if password incorrect
+     */
+    protected function verifyPassword($password)
+    {
+        if ($this->password == md5($password))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * <pre>
+     * <p>[Summary]</p>
+     * Validate Password
+     * </pre>
+     * @param string $password password
+     * @return boolean true: if password validate success
+     *                         false: if password validate fail
+     */
+    public function validatePassword($password)
+    {
+        //Check password must not contain any whitespace
+        $regexSpace = '/\s/';
+        if (true == preg_match($regexSpace, $password))
+        {
+            return false;
+        }
+
+        //Check password must be at least 6 characters long.
+        if (strlen($password) != 6)
+        {
+            return false;
+        }
+
+        //Check password must contain at least one uppercase, at least one lowercase letter, at least one digit and symbol.
+        $regexCharacter = '/^.*(?=.{7,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/';
+        if (false == preg_match($regexCharacter, $password))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * <pre>
+     * <p>[Summary]</p>
+     * Set New Password
+     * </pre>
+     * @param string $password password
+     * @return boolean true: if password validate success
+     *                         false: if password validate fail
+     */
+    public function setNewPassword($password)
+    {
+        $isValid = $this->validatePassword($password);
+        if(false === $isValid)
+        {
+            return false;
+        }
+        else
+        {
+            $this->password = md5($password);
+        }
+
+        return true;
+    }
 }
